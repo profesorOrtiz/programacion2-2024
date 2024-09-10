58a58fc88ca7a0d0134780abb842e7c98fbaa83e
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-09-10 17:30:09 -0300

    Ejercicios con el módulo math

---
+++ unidad4/math.py
@@ -0,0 +1,19 @@
+import math
+
+# Devuelve el numero entero superior
+print(math.ceil(3.5))
+# Devuelve el numero entero inferior
+print(math.floor(3.5))
+# Redondear un numero
+print(round(3.4999999))
+
+# Calcular el valor absoluto de un numero
+print(math.fabs(-5))
+# Calcular la potencia
+# Args: 1) numero base, 2) potencia a aplicar
+print(math.pow(2, 3))
+# Calcular la raiz cuadrada
+print(math.sqrt(9))
+
+print(math.pi)
+print(math.e)
